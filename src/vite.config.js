8d0456ad3a666124a858c2d9a77a26f7bd9e38fc
mino <81564697+umeshio@users.noreply.github.com>
2025-04-15 18:27:28 +0900

    認証機能パッケージBreeze導入

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    server: {
-        port: 5173,
-        strictPort: true,
-        hmr: {
-          host: 'localhost', // ホストPCからのアクセスURL
-        },
-    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
 });
